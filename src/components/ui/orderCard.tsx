fcedab1feb3462c738470e9ae32b68a6c608e455
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-26 22:11:23 +0700

    feat add props to render dynamic data

--- src/components/ui/orderCard.tsx
+++ src/components/ui/orderCard.tsx
@@ -1,36 +1,55 @@
+import formatDate from "@/utils/formatDate";
+import formatPriceToVND from "@/utils/formatPriceToVND";
+import formatTime from "@/utils/formatTime";
 import { BoxIcon, ClockIcon, EditIcon, EyeIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
-const OrderCard = () => {
+const OrderCard = ({
+  orderCode,
+  total_items,
+  total_amount,
+  dateTime,
+  statusOrder,
+}: {
+  orderCode: string;
+  total_items: number;
+  total_amount: number;
+  dateTime: number;
+  statusOrder: string;
+}) => {
   return (
     <div className="bg-white shadow rounded-md border border-gray-300 p-3">
       <div className="flex items-center justify-between pb-2 ">
         <div className="flex flex-col">
           <h3 className="font-semibold text-sm">Guest</h3>
-          <span className="text-sm opacity-80">#31458</span>
+          <span className="text-sm opacity-80">#{orderCode}</span>
         </div>
         <div className="flex   bg-[#daebfb] text-blue-600 items-center gap-2 rounded-full py-1 px-2">
           <ClockIcon className="size-4" />
-          <span className="text-[12px]  font-medium">Pending</span>
+          <span className="text-[12px]  font-medium">{statusOrder}</span>
         </div>
       </div>
       <div className="py-4  border-y border-gray-300 flex items-center justify-between">
         <div className="flex text-sm items-center gap-3">
           <BoxIcon className="size-6" />
-          <span>4 items</span>
+          <span>{total_items} items</span>
         </div>
         <div className="flex gap-2 items-center">
           <ClockIcon className="size-5" />
           <div className="flex flex-col gap-1 text-sm opacity-80">
             <span>Time</span>
-            <span className="font-medium text-black text-[15px]">03:14 PM</span>
-            <span>16/4/2025</span>
+            <span className="font-medium text-black text-[15px]">
+              {formatTime(new Date(dateTime))}
+            </span>
+            <span>{formatDate(new Date(dateTime))}</span>
           </div>
         </div>
       </div>
       <div className="flex items-center justify-between py-2 font-medium opacity-70 ">
         <span>Total amount:</span>
-        <span className=" text-orange-600 font-semibold">100.000d</span>
+        <span className=" text-orange-600 font-semibold">
+          {formatPriceToVND(total_amount)}
+        </span>
       </div>
       <div className="flex items-center gap-3 mt-4">
         <Link className="flex border font-medium text-[13px] border-gray-300 rounded-md w-full px-4 py-2 items-center gap-2">

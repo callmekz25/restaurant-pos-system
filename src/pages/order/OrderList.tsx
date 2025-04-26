9cbacad344428e045f27a867b78ed4a2a5ac4bd9
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-26 22:12:37 +0700

    page order list

--- src/pages/order/OrderList.tsx
+++ src/pages/order/OrderList.tsx
@@ -2,6 +2,120 @@ import OrderCard from "@/components/ui/orderCard";
 import STATUS_ORDER from "@/constants/status.order";
 
 const OrderList = () => {
+  const orderList = [
+    {
+      id: 1,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "pending",
+    },
+    {
+      id: 2,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "processing",
+    },
+    {
+      id: 3,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "pending",
+    },
+    {
+      id: 4,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "completed",
+    },
+    {
+      id: 5,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "completed",
+    },
+    {
+      id: 6,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "pending",
+    },
+    {
+      id: 7,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "pending",
+    },
+    {
+      id: 8,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "canceled",
+    },
+    {
+      id: 9,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "refuned",
+    },
+    {
+      id: 10,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "canceled",
+    },
+    {
+      id: 11,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "pending",
+    },
+    {
+      id: 10,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "processing",
+    },
+    {
+      id: 11,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "processing",
+    },
+    {
+      id: 12,
+      order_code: "31458",
+      total_items: 4,
+      createdAt: Date.now(),
+      total_amout: 100000,
+      order_status: "completed",
+    },
+  ];
   return (
     <>
       <div className="flex items-center justify-between">
@@ -20,15 +134,18 @@ const OrderList = () => {
         </div>
       </div>
       <div className=" grid grid-cols-4 gap-4 mt-4 ">
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {orderList.map((order) => {
+          return (
+            <OrderCard
+              statusOrder={order.order_status}
+              key={order.id}
+              orderCode={order.order_code}
+              total_amount={order.total_amout}
+              total_items={order.total_items}
+              dateTime={order.createdAt}
+            />
+          );
+        })}
       </div>
     </>
   );

b9ee1bb6827b21e1b50f7617fed664745cd39893
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-26 22:11:37 +0700

    style fix height

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,13 +3,13 @@ import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 const Layout = () => {
   return (
-    <>
+    <div className="relative">
       <Header />
-      <main className="bg-[#f7f7f7] px-30 py-5 min-h-[80vh] max-h-[80vh] overflow-y-auto">
+      <main className="bg-[#f7f7f7] px-30 py-5 min-h-screen ">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 

be14175013c978214a82d1bd2ed6d1aecf0690d5
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-26 22:13:24 +0700

    utils for format time to hh:mm

---
+++ src/utils/formatTime.ts
@@ -0,0 +1,11 @@
+const formatTime = (date: Date): string => {
+  let hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  return `${hours}:${minutes} ${ampm}`;
+};
+export default formatTime;

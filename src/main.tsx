fd57d3bb57a983a927976af50e62be140c0fb676
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-07 21:13:40 +0700

    feat config react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 30, // Giữ cache 30 phút, không fetch lại nếu chưa hết hạn
+      cacheTime: 1000 * 60 * 60, // Dữ liệu cache tồn tại 60 phút trước khi bị xoá
+      refetchOnWindowFocus: false, // Không fetch lại khi chuyển tab
+      refetchOnReconnect: false, // Không fetch lại khi mất mạng rồi có lại
+      refetchOnMount: false, // Không fetch lại khi component mount lại
+      retry: 2, // Retry API tối đa 2 lần nếu lỗi
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );

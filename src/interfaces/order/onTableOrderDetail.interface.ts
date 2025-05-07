087a1bcbe3c6165918c8402e6ab474e759f0d80d
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-07 21:16:42 +0700

    feat add interface on table order detail

---
+++ src/interfaces/order/onTableOrderDetail.interface.ts
@@ -0,0 +1,11 @@
+interface OnTableOrderDetail {
+  foodId: string;
+  foodName: string;
+  foodImage: string;
+  variantId: string;
+  variantName: string;
+  note: string;
+  amount: number;
+  actutalPrice: number;
+}
+export default OnTableOrderDetail;

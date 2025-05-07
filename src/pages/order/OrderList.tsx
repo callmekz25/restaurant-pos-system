f8b8b282d05472b490b821250479a68f94520d07
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-07 21:17:48 +0700

    feat add get all orders & render orders card

--- src/pages/order/OrderList.tsx
+++ src/pages/order/OrderList.tsx
@@ -1,121 +1,15 @@
+import Loading from "@/components/ui/loading";
 import OrderCard from "@/components/ui/orderCard";
 import STATUS_ORDER from "@/constants/status.order";
+import { useGetOrders } from "@/hooks/order";
+import OrderStatis from "@/interfaces/order/orderStatis.interface";
 
 const OrderList = () => {
-  const orderList = [
-    {
-      id: 1,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "pending",
-    },
-    {
-      id: 2,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "processing",
-    },
-    {
-      id: 3,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "pending",
-    },
-    {
-      id: 4,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "completed",
-    },
-    {
-      id: 5,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "completed",
-    },
-    {
-      id: 6,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "pending",
-    },
-    {
-      id: 7,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "pending",
-    },
-    {
-      id: 8,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "canceled",
-    },
-    {
-      id: 9,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "refuned",
-    },
-    {
-      id: 10,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "canceled",
-    },
-    {
-      id: 11,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "pending",
-    },
-    {
-      id: 10,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "processing",
-    },
-    {
-      id: 11,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "processing",
-    },
-    {
-      id: 12,
-      order_code: "31458",
-      total_items: 4,
-      createdAt: Date.now(),
-      total_amout: 100000,
-      order_status: "completed",
-    },
-  ];
+  const { data: orders, isLoading, error } = useGetOrders();
+  console.log(orders);
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <>
       <div className="flex items-center justify-between">
@@ -134,18 +28,20 @@ const OrderList = () => {
         </div>
       </div>
       <div className=" grid grid-cols-4 gap-4 mt-4 ">
-        {orderList.map((order) => {
-          return (
-            <OrderCard
-              statusOrder={order.order_status}
-              key={order.id}
-              orderCode={order.order_code}
-              total_amount={order.total_amout}
-              total_items={order.total_items}
-              dateTime={order.createdAt}
-            />
-          );
-        })}
+        {orders &&
+          orders.length > 0 &&
+          orders.map((order: OrderStatis) => {
+            return (
+              <OrderCard
+                statusOrder={order.status}
+                key={order.orderId}
+                orderCode={order.orderId}
+                total={order.total}
+                total_items={order.foods.length}
+                dateTime={order.timeIn}
+              />
+            );
+          })}
       </div>
     </>
   );

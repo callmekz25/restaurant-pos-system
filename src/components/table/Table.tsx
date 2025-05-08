9ef62cd2218815be2a732a5ff72b43043b26f782
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-08 22:01:27 +0700

    fix add status props

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,11 +1,15 @@
+import TableStatus from "@/enum/tableStatus";
+
 const Table = ({
   numberOfSeats,
   numberOfTable,
   defaultSize = 100,
+  status,
 }: {
   numberOfSeats: number;
-  numberOfTable: number;
+  numberOfTable: string;
   defaultSize?: number;
+  status: string;
 }) => {
   return (
     <div className="p-2 border border-gray-100 transition-all duration-300 cursor-pointer hover:border-blue-700  rounded-md">
@@ -24,9 +28,25 @@ const Table = ({
           })}
         </div>
         <div className="w-full py-10 rounded-lg bg-[#dcdcdc] flex items-center justify-center">
-          <div className="bg-[#c0d7ce] text-green-600 py-2 font-semibold px-3 rounded-md">
-            T{numberOfTable}
-          </div>
+          <span
+            className={` py-2 font-semibold px-3 rounded-md ${
+              status === TableStatus.AVAILABLE
+                ? "bg-[#c0d7ce] text-green-600"
+                : ""
+            } ${
+              status === TableStatus.RESERVED ? "bg-[#e7ccc6] text-red-600" : ""
+            } ${
+              status === TableStatus.OCCUPIED
+                ? "bg-[#c6cee7] text-blue-600"
+                : ""
+            }  ${
+              status === TableStatus.UNAVAILABLE
+                ? "bg-[#d4d5dc] text-gray-400"
+                : ""
+            }`}
+          >
+            {numberOfTable}
+          </span>
         </div>
         <div className="flex items-center gap-5">
           {Array.from({ length: numberOfSeats / 2 }).map((_, index) => {

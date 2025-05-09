a981f5b9c3c9c250b51b532c60bf6840dee67ba3
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-09 22:49:24 +0700

    feat add init time sheet

--- src/hooks/timeSheet.ts
+++ src/hooks/timeSheet.ts
@@ -1,10 +1,18 @@
-import { getTimeSheetByMonth } from "@/services/timeSheetService";
-import { useQuery } from "@tanstack/react-query";
+import {
+  getTimeSheetByMonth,
+  initTimeSheet,
+} from "@/services/timeSheetService";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useGetTimeSheetByMonth = (month: string, year: string) => {
   return useQuery({
     queryKey: ["time-sheets", month, year],
     queryFn: () => getTimeSheetByMonth(month, year),
     enabled: !!month && !!year,
+  });
+};
+export const useInitTimeSheet = () => {
+  return useMutation({
+    mutationFn: initTimeSheet,
   });
 };

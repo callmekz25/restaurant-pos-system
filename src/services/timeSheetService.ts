4087d02a0a4fa4a194e1895d1b592f2777c3d6a2
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-13 23:48:27 +0700

    feat add service to attendance

--- src/services/timeSheetService.ts
+++ src/services/timeSheetService.ts
@@ -1,4 +1,6 @@
-import httpRequest from '@/config/axios/axios.config';
+import httpRequest from "@/config/axios/axios.config";
+import IAttendanceRequest from "@/interfaces/timesheet/attendanceRequest";
+import ITimeSheet from "@/interfaces/timesheet/timeSheet.interface";
 
 export const getTimeSheetByMonth = async (month: string, year: string) => {
   try {
@@ -12,7 +14,19 @@ export const getTimeSheetByMonth = async (month: string, year: string) => {
 };
 export const initTimeSheet = async () => {
   try {
-    const { data } = await httpRequest.post('/timesheets/init-timesheet');
+    const { data } = await httpRequest.post("/timesheets/init-timesheet");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const attendance = async (attendanceRequest: IAttendanceRequest) => {
+  try {
+    const { data } = await httpRequest.post(
+      "/timesheets/attendance",
+      attendanceRequest
+    );
     return data;
   } catch (error) {
     console.log(error);
